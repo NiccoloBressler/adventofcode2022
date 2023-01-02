780042f50b127eb73c35c45ff36a1989c9e2cdd3
Niccolo Bressler <bresslerniccolo@gmail.com>
2023-01-02 11:13:35 -0500

    Day 3 Final

--- Day3/partTwo.py
+++ Day3/partTwo.py
@@ -6,18 +6,16 @@ def partTwo():
     priority_bank = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     sum = 0
 
-    for sack in data[:-2:]:
+    for sack in data[:-2:3]:
         second_sack = data[data.index(sack)+1]
         third_sack = data[data.index(sack)+2]
 
         for item in sack:
-            if item in second_sack:
-                if item in third_sack:
-                    sum += priority_bank.index(item) + 1
-                    break
+            if item in second_sack and item in third_sack:
+                sum += priority_bank.index(item) + 1
                 break
-            break
-    
+
     return sum
 
 print(partTwo())
+
